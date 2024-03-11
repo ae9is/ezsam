a558941c162bfb1ad3d9e83a2550859e6e963c98
ae9is <125031666+ae9is@users.noreply.github.com>
2024-03-11 07:48:29 -0700

    fix: cache folder location typo

--- src/ezsam/cli/config/defaults.py
+++ src/ezsam/cli/config/defaults.py
@@ -3,7 +3,7 @@ import pathlib
 from ezsam.cli.formats import OutputImageFormat, OutputVideoCodec
 
 HOME_FOLDER = pathlib.Path.home().as_posix()
-DEFAULT_CACHE_FOLDER_LOCATION = f'{HOME_FOLDER}/.config/ezsam'
+DEFAULT_CACHE_FOLDER_LOCATION = f'{HOME_FOLDER}/.cache/ezsam'
 DEFAULT_GROUNDING_DINO_CONFIG_PATH = f'{DEFAULT_CACHE_FOLDER_LOCATION}/GroundingDINO_SwinT_OGC.py'
 DEFAULT_SAM_MODEL = 'vit_h'
 DEFAULT_OUTPUT_DIR = '.'
